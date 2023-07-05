52ffc5b40a01dfbbec0bbc6e153c131e29b1e430
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2023-07-05 17:25:20 +0100

    feat: add graceful shutdown to app & implement Drop on ThreadPool

--- web-server/src/thread_pool/worker.rs
+++ web-server/src/thread_pool/worker.rs
@@ -7,20 +7,31 @@ use std::{
 use super::thread_pool::Job;
 
 pub struct Worker {
-    id: usize,
-    handler: thread::JoinHandle<()>,
+    pub id: usize,
+    pub handler: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> io::Result<Worker> {
         let builder = thread::Builder::new();
         let handler = builder.spawn(move || loop {
-            let job = receiver.lock().expect("Mutex is in poisened state. It's possible this is caused by other threads panicking.").recv().unwrap();
-            println!("[Worker {id}] Starting execution of new job...");
-            job();
-            println!("[Worker {id}] Finished execution of job. Waiting for new work...");
+            let message = receiver.lock().expect("Mutex is in poisened state. It's possible this is caused by other threads panicking.").recv();
+            match message {
+                Ok(job) => {
+                    println!("[Worker {id}] Starting execution of new job...");
+                    job();
+                    println!("[Worker {id}] Finished execution of job. Waiting for new work...");
+                }
+                Err(_) => {
+                    println!("[Worker {id}] Disconnected. Shutting down.");
+                    break;
+                }
+            }
         })?;
 
-        Ok(Worker { id, handler })
+        Ok(Worker {
+            id,
+            handler: Some(handler),
+        })
     }
 }
